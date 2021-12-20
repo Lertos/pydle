6f03ba35e1073ea2f300cee4fccf83ce94bcef20
Lertos <deetasticgaming@gmail.com>
2021-12-19 21:31:33 -0400

    Adding the key handler, fps clock, draw method, and cleaning up code

--- main.py
+++ main.py
@@ -1,49 +1,62 @@
 import pygame
 import pygame.pkgdata
-import src.test
+
+import src.key_handler
+
 from pygame.locals import *
+from time import sleep
+
+
+WIDTH = 1280
+HEIGHT = 720
+
+FPS = 10
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+#Initialise the screen
+pygame.init()
+
+FLAGS = pygame.SCALED | pygame.RESIZABLE
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT), FLAGS, vsync=1)
+pygame.display.set_caption("Noxphor")
+
+TEXT_DATA = pygame.pkgdata.getResource("freesansbold.ttf")
+TEXT_FONT = pygame.font.Font(TEXT_DATA, 24)
+
+
+
+
+def draw():
+    WINDOW.fill(BLACK)
+    text = TEXT_FONT.render("Test Text", 1, WHITE)
+    textpos = text.get_rect()
+    textpos.centerx = WINDOW.get_rect().centerx
+    textpos.centery = WINDOW.get_rect().centery
+    WINDOW.blit(text, textpos)
+
+    pygame.display.update()
+
 
 def main():
-
-    WIDTH = 1280
-    HEIGHT = 720
-
-    tester = src.test
-
-    text = tester.test()
-    # Initialise screen
-    pygame.init()
-    flags = pygame.SCALED | pygame.RESIZABLE
-    screen = pygame.display.set_mode((WIDTH, HEIGHT), flags, vsync=1)
-    pygame.display.set_caption("Noxphor")
-
-    # Fill background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((0, 0, 0))
-
-    # Display some text
-    font_data = pygame.pkgdata.getResource("freesansbold.ttf")
-    font = pygame.font.Font(font_data, 36)
-    text = font.render(text, 1, (250, 250, 250))
-    textpos = text.get_rect()
-    textpos.centerx = background.get_rect().centerx
-    textpos.centery = background.get_rect().centery
-    background.blit(text, textpos)
-
-    # Blit everything to the screen
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
-
-    # Event loop
+    #Ensures that the frames per second are enforced
+    fpsClock = pygame.time.Clock()
+    
+    #Game loop
     while 1:
+        fpsClock.tick(FPS)
+        
         for event in pygame.event.get():
             if event.type == QUIT:
-                print('cya')
                 return
 
-        screen.blit(background, (0, 0))
-        pygame.display.flip()
+        #Get the key presses
+        keys_pressed = pygame.key.get_pressed()
+
+
+        draw()
+        
 
 
 if __name__ == "__main__": main()
